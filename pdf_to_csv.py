5c6ac56b213a10c6cce035e2bbdefc1a61dc0be0
Elias <elias.regopoulos@gmail.com>
2020-09-07 17:05:21 +0200

    PDF to CSV

--- pdf_to_csv.py
+++ pdf_to_csv.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-# Invocation: ./pdf_to_csv.py PDF_SOURCE GRADUATION_TIME
+# Invocation: ./pdf_to_csv.py PDF_SOURCE
 # Arguments:
 #   PDF_SOURCE: PDF file with table to be converted
-#   GRADUATION_TIME: String indicating the graduation time, in "YEAR.MONTH" format
 # Usage: Takes a PDF containing a (multi-page) table of graduating students, and extracts it to a csv file.
 
 import sys
@@ -11,22 +10,23 @@ import pandas as pd
 import camelot
 import re
 
+print(sys.argv[1])
+
 if len(sys.argv)<=1 :
     sys.exit("PDF file expected as argument.")
 elif not os.path.isfile(sys.argv[1]):
     sys.exit("PDF file does not exist.")
-elif len(sys.argv)<=2:
-    sys.exit("Graduation time missing.")
 
 pdf_file = sys.argv[1]
-graduation_date = sys.argv[2]
 
 # Read tables into DataFrame
 tables = camelot.read_pdf(pdf_file, pages='all')
 table_frame = pd.concat([table_page.df for table_page in tables])
 table_frame.reset_index(inplace=True,drop=True)
-# Remove header row, set proper row names
-table_frame.columns = ['#','ID','Surname','Name','Father\'s name']
+# Set proper column names, remove header row
+table_frame.columns = table_frame.iloc[0]
+table_frame = table_frame[["Α.Μ.", "Επώνυμο", "Όνομα", "Πατρώνυμο"]] # drop possible extra columns
+table_frame.columns = ['ID','Surname','Name','Father\'s name']
 table_frame.drop([0],inplace=True)
 # Remove inline newlines from text
 nl = re.compile('\n')
@@ -36,5 +36,6 @@ table_frame['Name'] = [nl.sub('', x) for x in table_frame['Name']]
 table_frame['Father\'s name'] = [nl.sub('', x) for x in table_frame['Father\'s name']]
 
 # Write DataFrame to csv
-table_frame.to_csv(graduation_date, index=False, header=False)
+csv_file = os.path.splitext(pdf_file)[0] + ".csv"
+table_frame.to_csv(csv_file, index=False, header=False)
 
--- unipi_grades.sh
+++ unipi_grades.sh
@@ -8,28 +8,48 @@ announcements_dir="announcements"
 # 2. Convert files to PDFs
 for announcement_file in $announcements_dir/*; do
 	# Skip files already generated from previous runs
-	echo "$announcement_file"
 	if [[ $announcement_file == *.pdf ]]
 	then
-		echo "skipping pdf"
 		continue
 	fi
 
+	# Determine original file type
 	file_type="$(file -b "$announcement_file" | sed 's/,.*//g')"
-	# No need to convert HTML files.
 	case $file_type in
 		"Composite Document File V2 Document"|"Microsoft Word 2007+")
-			lowriter --headless --convert-to pdf --outdir "$announcements_dir" $announcement_file
+			lowriter --headless --convert-to pdf --outdir "$announcements_dir" $announcement_file > /dev/null 2>&1
 			;;
 		"PDF document")
 			cp "$announcement_file" "$announcement_file.pdf"
-
+			;;
+		# No need to convert HTML files.
 	esac
 done
 
+# 3. Filter announcements that include the proper template
+undergrad_template="undergrad.template"
+for announcement_file in $announcements_dir/*.pdf; do
+	is_undergrad=$(pdf2txt $announcement_file | grep -f $undergrad_template)
+	if [ -z "$is_undergrad" ]; then
+		rm "$announcement_file"
+	fi
+done
 
-
-# Extract table to CSV
-for pdf_file in "$announcements_dir/*.pdf"; do
-	./pdf_to_csv.py "$pdf_file" "${pdf_file%:*}"
+# 4. Extract graduation date
+last_year=0
+graduations_in_year=0
+for announcement_file in $announcements_dir/*.pdf; do
+	year=$(pdf2txt "$announcement_file" | grep -Eo -f undergrad_date.template | awk '{print $NF}')
+	if (( year > last_year )); then
+		graduations_in_year=0
+	else
+		graduations_in_year=$((graduations_in_year+1))
+	fi
+	last_year=$year
+	mv "$announcement_file" "$announcements_dir"/"$year"."$graduations_in_year"."pdf"
+done
+	
+# 5. Extract table to CSV
+for pdf_file in "$announcements_dir"/*.pdf; do
+	./pdf_to_csv.py "$pdf_file"
 done
